6049d5e216b76913ec5cdfb3f34b48dc7dfe1333
Lance Riedel <lanceriedel@gmail.com>
2014-07-30 10:11:31 -0700

    more cleanup

--- oozie/adcreatorworkflow/deploy-adcreatorworkflow.sh
+++ oozie/adcreatorworkflow/deploy-adcreatorworkflow.sh
@@ -1,2 +1,2 @@
-cp  ../../target/hadoop-job-demo-1.0-SNAPSHOT-job.jar ./lib/
+cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib
 hadoop fs -rm -R kosei/adcreator/oozie/adcreatorworkflow;hadoop fs -put ../adcreatorworkflow kosei/adcreator/oozie/
