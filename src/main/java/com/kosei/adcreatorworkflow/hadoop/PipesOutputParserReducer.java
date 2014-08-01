7a1450ecd52e13ed1011dea2be313730aaad51ca
chantat <c.eksombatchai@gmail.com>
2014-08-01 12:52:10 -0700

    decompose code

--- src/main/java/com/kosei/adcreatorworkflow/hadoop/PipesOutputParserReducer.java
+++ src/main/java/com/kosei/adcreatorworkflow/hadoop/PipesOutputParserReducer.java
@@ -4,7 +4,6 @@ import com.kosei.proto.AdComponentsMessages.AdComponents;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -17,9 +16,6 @@ import java.util.Arrays;
 
 import org.apache.commons.io.IOUtils;
 
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
-
 /**
  * @author root
  */
@@ -28,20 +24,28 @@ public class PipesOutputParserReducer extends
 
     @Override
     public void reduce(NullWritable key, Iterable<BytesWritable> values, Context context)
-
             throws IOException, InterruptedException {
 
         String tarOutput = context.getConfiguration().get("tar.output");
         Path file = new Path(tarOutput);
         FileSystem hdfs = FileSystem.get(file.toUri(), context.getConfiguration());
+
         if (hdfs.exists(file)) { hdfs.delete(file, true); }
 
         FSDataOutputStream out = hdfs.create(file);
+        createTar(values, out);
+        out.close();
+
+        context.write(NullWritable.get(), NullWritable.get());
+    }
+
+    private void createTar(Iterable<BytesWritable> values, DataOutputStream out)
+            throws IOException, InterruptedException {
         BufferedOutputStream bufferedOut = new BufferedOutputStream(out);
         GzipCompressorOutputStream gzOut = new GzipCompressorOutputStream(bufferedOut);
         TarArchiveOutputStream tarOut = new TarArchiveOutputStream(gzOut);
 
-        for(BytesWritable value : values) {
+        for (BytesWritable value : values) {
             AdComponents adComponents = AdComponents.parseFrom(getValidBytes(value));
 
             for (int i = 0; i < adComponents.getGeneratedAdsCount(); i++) {
@@ -61,9 +65,6 @@ public class PipesOutputParserReducer extends
         tarOut.close();
         gzOut.close();
         bufferedOut.close();
-        out.close();
-
-        context.write(NullWritable.get(), NullWritable.get());
     }
 
     public byte[] getValidBytes(BytesWritable bw) {
