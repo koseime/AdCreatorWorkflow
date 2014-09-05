b867728972e2a1070fc0a4c97b50af51c7504c0d
Pong Eksombatchai <pong@desktop-foa.service.altiscale.com>
2014-09-05 18:01:57 +0000

    separated config files for local and dev

---
+++ oozie/create-campaign/deploy-create-campaign-alti.sh
@@ -0,0 +1,7 @@
+rm lib/*
+cp  ../../../kosei-management-backend/client/build/libs/management-client-*-standalone.jar ./lib/
+cp ../../../AdCreator/ad-creator bin/ad-creator
+cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib
+hadoop fs -rm -R hdfs://nn-foa.s3s.altiscale.com:8020/shared/kosei/adcreator/oozie/create-campaign;hadoop fs -put ../create-campaign hdfs://nn-foa.s3s.altiscale.com:8020/shared/kosei/adcreator/oozie
+hadoop fs -chmod 777 hdfs://nn-foa.s3s.altiscale.com:8020/shared/kosei/adcreator/oozie/create-campaign/lib/*
+hadoop fs -chmod 777 hdfs://nn-foa.s3s.altiscale.com:8020/shared/kosei/adcreator/oozie/create-campaign/bin/*
