e19b9d05ddd1289d19ec141ac8ad00b084acaa7c
Pong Eksombatchai <pong@desktop-foa.service.altiscale.com>
2014-09-05 18:06:38 +0000

    added to deploy script

--- oozie/create-campaign/deploy-create-campaign-alti.sh
+++ oozie/create-campaign/deploy-create-campaign-alti.sh
@@ -1,4 +1,5 @@
 rm lib/*
+cp coord-config-default-alti.xml coord-config-default.xml
 cp  ../../../kosei-management-backend/client/build/libs/management-client-*-standalone.jar ./lib/
 cp ../../../AdCreator/ad-creator bin/ad-creator
 cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib
