7509be1eb96937a376ca93f173ade3b652a38f46
chantat <c.eksombatchai@gmail.com>
2014-09-04 23:59:54 -0700

    removed ImageCrawlerMapper and added BundleCreativeAsset to workflow

--- oozie/create-campaign/deploy-create-campaign.sh
+++ oozie/create-campaign/deploy-create-campaign.sh
@@ -1,4 +1,4 @@
-cp  ../../../kosei-management-backend/client/build/libs/management-client-0.5-standalone.jar ./lib/
+cp  ../../../kosei-management-backend/client/build/libs/management-client-0.10-standalone.jar ./lib/
 cp ../../../AdCreator/ad-creator bin/ad-creator
 cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib
 hadoop fs -rm -R kosei/adcreator/oozie/create-campaign;hadoop fs -put ../create-campaign kosei/adcreator/oozie/
