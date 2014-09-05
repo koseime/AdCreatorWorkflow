c6100019c1083cbba26da8432d33af7d41825316
chantat <c.eksombatchai@gmail.com>
2014-09-05 14:54:47 -0700

    changed create-campaign to get resource from S3

--- oozie/create-campaign/deploy-create-campaign.sh
+++ oozie/create-campaign/deploy-create-campaign.sh
@@ -1,4 +1,5 @@
-cp  ../../../kosei-management-backend/client/build/libs/management-client-0.10-standalone.jar ./lib/
-cp ../../../AdCreator/ad-creator bin/ad-creator
+rm lib/*
+cp  ../../../kosei-management-backend/client/build/libs/management-client-*-standalone.jar ./lib/
+cp ../../../AdCreator/native/ad-creator bin/ad-creator
 cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib
 hadoop fs -rm -R kosei/adcreator/oozie/create-campaign;hadoop fs -put ../create-campaign kosei/adcreator/oozie/
--- oozie/process-catalog/deploy-process-catalog.sh
+++ oozie/process-catalog/deploy-process-catalog.sh
@@ -1,3 +1,3 @@
-cp  ../../../kosei-management-backend/client/build/libs/management-client-0.5-standalone.jar ./lib/
+cp  ../../../kosei-management-backend/client/build/libs/management-client-*-standalone.jar ./lib/
 cp  ../../build/libs/ad-creator-workflow-0.1-SNAPSHOT-all.jar ./lib/
 hadoop fs -rm -R kosei/adcreator/oozie/process-catalog;hadoop fs -put ../process-catalog kosei/adcreator/oozie/
