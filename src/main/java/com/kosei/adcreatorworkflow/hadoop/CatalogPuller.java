7fe1079bb62f59e5d4d14a6b2b7b7606a70b7956
chantat <c.eksombatchai@gmail.com>
2014-10-17 17:31:06 -0700

    added java action to submit prepare jobs

---
+++ src/main/java/com/kosei/adcreatorworkflow/hadoop/CatalogPuller.java
@@ -0,0 +1,75 @@
+package com.kosei.adcreatorworkflow.hadoop;
+
+import com.kosei.dropwizard.management.api.JsonCatalogVersion;
+import com.kosei.dropwizard.management.api.JsonCatalogVersionPrepareJob;
+import com.kosei.dropwizard.management.client.ManagementJsonClient;
+import com.kosei.dropwizard.management.client.resources.CatalogVersionPrepareJobResource;
+import com.kosei.dropwizard.management.client.resources.CatalogVersionResource;
+import io.dropwizard.jackson.Jackson;
+import retrofit.client.UrlConnectionClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ *
+ */
+public class CatalogPuller {
+
+    public static void main(String args[]) throws IOException {
+        String apiToken = args[0];
+        String baseURL = args[1];
+
+        String oozieJobId = System.getProperty("oozie.job.id");
+        if (oozieJobId == null) { throw new RuntimeException("oozie.job.id not defined"); }
+
+        ManagementJsonClient managementJsonClient = new ManagementJsonClient(
+                apiToken, baseURL, Jackson.newObjectMapper(), new UrlConnectionClient());
+
+        CatalogVersionPrepareJobResource catalogVersionPrepareJobResource =
+                managementJsonClient.getCatalogVersionPrepareJobResource();
+
+        CatalogVersionResource catalogVersionResource = managementJsonClient.getCatalogVersionResource();
+
+        JsonCatalogVersionPrepareJob newJob = catalogVersionPrepareJobResource.create(
+                new JsonCatalogVersionPrepareJob(-1, oozieJobId, null, null, null));
+
+        List<JsonCatalogVersion> catalogVersions =
+                catalogVersionResource.viewAllByCatalogVersionPrepareJobId(newJob.id, 0, Integer.MAX_VALUE).content;
+
+        StringBuilder catalogLocations = new StringBuilder();
+        StringBuilder catalogNames = new StringBuilder();
+        for (JsonCatalogVersion catalogVersion : catalogVersions) {
+            if (catalogLocations.length() != 0) {
+                catalogLocations.append(",");
+                catalogNames.append(",");
+            }
+            String catalogName = catalogVersion.advertiserId + "-" + catalogVersion.timestamp + "-" +
+                    catalogVersion.catalogId + "-" + catalogVersion.id + ".raw_catalog";
+            catalogLocations.append(catalogVersion.catalogLocation);
+            catalogNames.append(catalogName);
+        }
+
+        String propertiesFile = System.getProperty("oozie.action.output.properties");
+        if (propertiesFile != null) {
+            File propFile = new File(propertiesFile);
+
+            Properties properties = new Properties();
+            properties.setProperty("CATALOG_LOCATIONS", catalogLocations.toString());
+            properties.setProperty("CATALOG_NAMES", catalogNames.toString());
+            properties.setProperty("TIMESTAMP", Long.toString(System.currentTimeMillis()));
+
+            OutputStream os = new FileOutputStream(propFile);
+            properties.store(os, "");
+            os.close();
+        } else {
+            throw new RuntimeException("oozie.action.output.properties not defined");
+        }
+    }
+
+}
